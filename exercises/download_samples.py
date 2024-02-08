42eb8d3e4d75115b5880d7e9be28d1afe74f5689
Christian Studer <christian.studer@circl.lu>
2024-02-08 23:36:43 +0100

    chg: [exercise] Updated the samples downloading script

--- exercises/download_samples.py
+++ exercises/download_samples.py
@@ -4,25 +4,38 @@ from zipfile import ZipFile
 
 current_path = Path(__file__).resolve().parent
 
+filenames = (
+    '2024-01-19-GootLoader-infection-traffic.pcap',
+    '2024-01-23-UltraVNC-infection-traffic.pcap',
+    '2024-01-30-DarkGate-infection-traffic.pcap'
+)
+
 name = 'traffic-analysis'
-for date_value in ('2021-08-19', '2021-09-10', '2022-01-07', '2022-02-23', '2022-03-21'):
-    yyyy, mm, dd = date_value.split('-')
-    filename = f'{date_value}-{name}-exercise.pcap'
-    if (current_path / 'data' / filename).exists():
-        print(f'data/{filename} already downloaded and unzipped!')
-        continue
-    url = f'https://www.malware-{name}.net/{yyyy}/{mm}/{dd}/{filename}.zip'
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(current_path / 'data' / f'{filename}.zip', 'wb') as f:
-            f.write(response.content)
-        print(f'data/{filename}.zip - Successfully downloaded')
+for filename in filenames:
+    yyyy, mm, dd = filename.split('-')[:3]
+    zipped = current_path / 'data' / f'{filename}.zip'
+    if zipped.exists():
+        print(f'data/{filename}.zip already downloaded')
     else:
-        print(f'Error while downloading from {url}:\n{response.status_code} - {response.reason}')
+        url = f'https://www.malware-{name}.net/{yyyy}/{mm}/{dd}/{filename}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(current_path / 'data' / filename, 'wb') as f:
+                f.write(response.content)
+            print(f'data/{filename}.zip - Successfully downloaded')
+        else:
+            print(f'Error while downloading from {url}:\n{response.status_code} - {response.reason}')
+            continue
+    unzipped = current_path / 'data' / filename
+    if unzipped.exists():
+        print(f'data/{filename} already unzipped')
         continue
     try:
         with ZipFile(current_path / 'data' / f'{filename}.zip', 'r') as zf:
-            zf.extractall(current_path / 'data', pwd=bytes('infected', 'utf-8'))
+            zf.extractall(
+                current_path / 'data',
+                pwd=bytes(f'infected_{yyyy}{mm}{dd}', 'utf-8')
+            )
         print(f'    - Successfully unzipped')
     except Exception as e:
         print(f'An exception appeared during the unzipping process: {e}')
